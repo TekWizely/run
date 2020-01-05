3792b873f49e40b5e6983f4144709a56e51fc4ad
TekWize.ly <TekWize.ly@gmail.com>
2020-01-05 15:48:16 -0800

    Updates version string wity BuildTool/builder

--- version.go
+++ version.go
@@ -6,18 +6,23 @@ import (
 
 // Version stores the version tag - Should include leading 'v' - Update before tagging new versions.
 //
-var Version = "v0.6.5"
+var Version = "v0.6.6"
 
 // BuildDate is optional and can be set using '-ldflags "-X 'main.BuildDate=..."'.
 //
 var BuildDate string
 
-// GitSummary is optional and can be set using '-ldflags "-X 'main.BuildDate=..."'.
+// GitSummary is optional and can be set using '-ldflags "-X 'main.GitSummary=..."'.
+// Generally meant to contain the value of:
+//   git describe --tags --dirty --always
 //
 var GitSummary string
 
+// BuildTool is optional and can be set using '-ldflags "-X 'main.BuildTool=..."'.
+var BuildTool string
+
 // versionString generates a version string from available vars.
-// Variable names are compatible with govvv
+// Variable names are compatible with govvv where applicable
 //
 func versionString() string {
 	version := strings.Builder{}
@@ -26,27 +31,40 @@ func versionString() string {
 	//
 	version.WriteString(Version)
 
-	// BuildDate / GitSummary
+	// Extras
 	//
-	if len(BuildDate) > 0 || len(GitSummary) > 0 {
+	if len(BuildDate) > 0 || len(GitSummary) > 0 || len(BuildTool) > 0 {
 		if version.Len() > 0 {
 			version.WriteString(" ")
 		}
 		version.WriteString("(")
+		needsSpace := false
 		// Git Summary
 		//
 		if len(GitSummary) > 0 {
 			version.WriteString("build=")
 			version.WriteString(GitSummary)
+			needsSpace = true
 		}
 		// Build Date
 		//
 		if len(BuildDate) > 0 {
-			if len(GitSummary) > 0 {
+			if needsSpace {
 				version.WriteString(" ")
 			}
 			version.WriteString("date=")
 			version.WriteString(BuildDate)
+			needsSpace = true
+		}
+		// Build Tool
+		//
+		if len(BuildTool) > 0 {
+			if needsSpace {
+				version.WriteString(" ")
+			}
+			version.WriteString("builder=")
+			version.WriteString(BuildTool)
+			// needsSpace = true
 		}
 		version.WriteString(")")
 	}

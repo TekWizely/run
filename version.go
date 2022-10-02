a33c01423434aa9bd8fee52b270a67ea2889796c
TekWize.ly <TekWize.ly@gmail.com>
2022-10-01 22:17:03 -0700

    build: Prepares v0.9.1

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ import (
 
 // Version stores the version tag - Should include leading 'v' - Update before tagging new versions.
 //
-var Version = "v0.9.0"
+var Version = "v0.9.1"
 
 // BuildDate is optional and can be set using '-ldflags "-X 'main.BuildDate=..."'.
 //
